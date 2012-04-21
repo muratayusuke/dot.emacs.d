684d5e04132f4c36165988d4bd67ce73d72f913e
Yusuke Murata <info@muratayusuke.com>
2012-04-21 16:38:53 +0900

    enable gtags-find-tag-from-here

--- inits/101_init-gtags.el
+++ inits/101_init-gtags.el
@@ -36,7 +36,7 @@
       (goto-line line)
       (gtags-mode 1))))
 
-(define-key gtags-mode-map "\M-." 'gtags-find-tag)
+(define-key gtags-mode-map "\M-." 'gtags-find-tag-from-here)
 (define-key gtags-mode-map "\M-," 'gtags-find-rtag)
 (define-key gtags-mode-map "\M-s" 'gtags-find-symbol)
 (define-key gtags-mode-map "\M-/" 'gtags-find-pattern)
