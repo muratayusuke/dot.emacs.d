9821037f461c7a3c70d2ee5dc89911cbc45ec028
Yusuke Murata <info@muratayusuke.com>
2012-04-21 12:26:23 +0900

    enable php-mode

---
+++ init.el
@@ -0,0 +1,10 @@
+;; load-path
+(add-to-list 'load-path "~/.emacs.d/elisp")
+;(add-to-list 'load-path "~/.emacs.d/auto-install")
+
+;; init-loader
+;; http://coderepos.org/share/browser/lang/elisp/init-loader/init-loader.el
+;; デフォルトで"~/.emacs.d/inits"以下のファイルをロードする
+(require 'init-loader)
+(setq init-loader-show-log-after-init nil)
+(init-loader-load)
