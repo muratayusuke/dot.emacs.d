fd38ef6e90b07463021a13120154893da89934b2
Yusuke Murata <info@muratayusuke.com>
2012-04-22 17:02:28 +0900

    add some settings

--- init.el
+++ init.el
@@ -1,3 +1,7 @@
+;; 使い方：~/.emacs内にこのファイルを読み込むよう記載する
+;; (load "~/.emacs.d/init.el")
+
+
 ;; load-path
 (add-to-list 'load-path "~/.emacs.d/elisp")
 ;(add-to-list 'load-path "~/.emacs.d/auto-install")
@@ -8,3 +12,11 @@
 (require 'init-loader)
 (setq init-loader-show-log-after-init nil)
 (init-loader-load)
+
+;; 行番号表示
+(require 'wb-line-number)
+(wb-line-number-toggle)
+
+;; 選択部分をハイライト
+(transient-mark-mode t)
+
