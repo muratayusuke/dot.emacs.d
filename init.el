ad378ce14f814bd5501f4a990c4b8bdd2b131293
Yusuke Murata <info@muratayusuke.com>
2012-04-28 09:57:15 +0900

    enable auto-install and anything

--- init.el
+++ init.el
@@ -4,7 +4,7 @@
 
 ;; load-path
 (add-to-list 'load-path "~/.emacs.d/elisp")
-;(add-to-list 'load-path "~/.emacs.d/auto-install")
+(add-to-list 'load-path "~/.emacs.d/auto-install")
 
 ;; init-loader
 ;; http://coderepos.org/share/browser/lang/elisp/init-loader/init-loader.el
@@ -20,3 +20,8 @@
 ;; 選択部分をハイライト
 (transient-mark-mode t)
 
+;; TAB幅の設定
+(setq-default tab-width 4)
+(setq default-tab-width 4)
+(setq tab-stop-list '(4 8 12 16 20 24 28 32 36 40 44 48 52 56 60
+                      64 68 72 76 80 84 88 92 96 100 104 108 112 116 120))
---
+++ inits/002_init-auto-install.el
@@ -0,0 +1,6 @@
+;; (install-elisp "http://www.emacswiki.org/emacs/download/auto-install.el")
+(when (require 'auto-install nil t)
+  (setq auto-install-directory "~/.emacs.d/elisp/")
+  (auto-install-update-emacswiki-package-name t)
+  (auto-install-compatibility-setup))
+  (setq auto-install-use-wget nil)
