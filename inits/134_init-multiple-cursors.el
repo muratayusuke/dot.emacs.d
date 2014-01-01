4048889077c7df30bca13d9ad9f900da28f3da2f
muratayusuke <info@muratayusuke.com>
2014-01-01 21:32:35 +0900

    add multiple-cursors

---
+++ inits/134_init-multiple-cursors.el
@@ -0,0 +1,18 @@
+(add-to-list 'load-path "~/.emacs.d/elisp/multiple-cursors/")
+(require 'multiple-cursors)
+
+(global-set-key (kbd "C-S-c C-S-c") 'mc/edit-lines)
+(global-set-key (kbd "C->") 'mc/mark-next-like-this)
+(global-set-key (kbd "C-<") 'mc/mark-previous-like-this)
+(global-set-key (kbd "C-c C-<") 'mc/mark-all-like-this)
+
+(setq mc/cmds-to-run-for-all
+      '(
+		c-electric-delete-forward
+		ibus-handle-event
+		kill-region
+		))
+
+(setq mc/cmds-to-run-once
+      '(
+		))
