b3c116eded2b8c29bc3d77e319579a057e851e4a
muratayusuke <info@muratayusuke.com>
2014-01-01 22:52:17 +0900

    add key bind of yank-rectangle

--- inits/134_init-multiple-cursors.el
+++ inits/134_init-multiple-cursors.el
@@ -5,6 +5,7 @@
 (global-set-key (kbd "C->") 'mc/mark-next-like-this)
 (global-set-key (kbd "C-<") 'mc/mark-previous-like-this)
 (global-set-key (kbd "C-c C-<") 'mc/mark-all-like-this)
+(global-set-key (kbd "C-x SPC") 'set-rectangular-region-anchor)
 
 (setq mc/cmds-to-run-for-all
       '(
