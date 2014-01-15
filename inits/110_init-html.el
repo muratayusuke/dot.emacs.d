ed6a49e7b740db4e7b5f7e90314f72c7d512eb88
muratayusuke <info@muratayusuke.com>
2014-01-15 18:54:26 +0900

    enable gtags-mode in js2-mode

--- inits/110_init-html.el
+++ inits/110_init-html.el
@@ -10,3 +10,6 @@
 (autoload 'js2-mode "js2-mode" nil t)
 (add-to-list 'auto-mode-alist '("\\.js$" . js2-mode))
 (add-to-list 'auto-mode-alist '("\\.json$" . js2-mode))
+(defun my-js2-mode-hook()
+  (gtags-mode 1))
+(add-hook 'js2-mode-hook 'my-js2-mode-hook)
