25d016ddbbe7f7f140e5bbe06465a2dc4ed476b9
muratayusuke <info@muratayusuke.com>
2014-03-10 17:46:21 +0900

    introduce package.el and switch init-loader to use package

--- init.el
+++ init.el
@@ -1,15 +1,51 @@
 ;; 使い方：~/.emacs内にこのファイルを読み込むよう記載する
 ;; (load "~/.emacs.d/init.el")
 
+;; package configuration
+(require 'package)
+(setq package-user-dir "~/.emacs.d/elisp/elpa/")
+(add-to-list 'package-archives
+  '("melpa" . "http://melpa.milkbox.net/packages/") t)
+(package-initialize)
+
+(require 'cl)
+
+;; install packages
+(defvar my-installing-package-list
+  '(
+    ;; init-loader
+    init-loader
+
+    ;; for package compile
+    dash
+    f
+    ))
+
+(let ((not-installed (loop for x in my-installing-package-list
+                           when (not (package-installed-p x))
+                           collect x)))
+  (when not-installed
+    (package-refresh-contents)
+    (dolist (pkg not-installed)
+      (package-install pkg))))
+
+(require 'dash)
+(require 'f)
+
+(defun was-compiled-p (path)
+  "Does the directory at PATH contain any .elc files?"
+  (--any-p (f-ext? it "elc") (f-files path)))
+
+(defun ensure-packages-compiled ()
+  "If any packages installed with package.el aren't compiled yet, compile them."
+  (--each (f-directories package-user-dir)
+    (unless (was-compiled-p it)
+      (byte-recompile-directory it 0))))
+
+(ensure-packages-compiled)
+
 ;; デバッグ
 (setq debug-on-error t)
-
-;; load-path
-(add-to-list 'load-path "~/.emacs.d/auto-install")
-(let ((default-directory (expand-file-name "~/.emacs.d/elisp")))
-  (add-to-list 'load-path default-directory)
-  (if (fboundp 'normal-top-level-add-subdirs-to-load-path)
-      (normal-top-level-add-subdirs-to-load-path)))
 
 ;; init-loader
 ;; http://coderepos.org/share/browser/lang/elisp/init-loader/init-loader.el
@@ -17,6 +53,13 @@
 (require 'init-loader)
 (setq init-loader-show-log-after-init nil)
 (init-loader-load)
+
+;; load-path
+(add-to-list 'load-path "~/.emacs.d/auto-install")
+(let ((default-directory (expand-file-name "~/.emacs.d/elisp")))
+  (add-to-list 'load-path default-directory)
+  (if (fboundp 'normal-top-level-add-subdirs-to-load-path)
+      (normal-top-level-add-subdirs-to-load-path)))
 
 ;; 行番号表示
 ;(require 'wb-line-number)
