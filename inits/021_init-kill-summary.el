1f3389a4e064b8d2ddc49f4883335325e2c49511
muratayusuke <info@muratayusuke.com>
2014-03-11 11:31:20 +0900

    replace kill-summary to helm-show-kill-ring

--- inits/021_init-kill-summary.el
+++
@@ -1,2 +0,0 @@
-(autoload 'kill-summary "kill-summary" nil t)
-(define-key global-map "\ey" 'kill-summary)
--- inits/10-helm.el
+++ inits/10-helm.el
@@ -1,3 +1,4 @@
 (require 'helm-config)
 (helm-mode t)
 (global-set-key "\M-x" 'helm-M-x)
+(global-set-key "\M-y" 'helm-show-kill-ring)
