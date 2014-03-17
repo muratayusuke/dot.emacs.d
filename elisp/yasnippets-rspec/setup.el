9dfde0d56e88bed21bb14adffd41fa824d6c14e3
muratayusuke <info@muratayusuke.com>
2014-03-18 07:16:58 +0900

    remove yasnippets

--- elisp/yasnippets-rspec/setup.el
+++
@@ -1,8 +0,0 @@
-;;; setup.el --- setup yasnippets for use with rspec
-(require 'yasnippet)
-(yas/initialize)
-(yas/load-directory
- (concat (file-name-directory (or load-file-name buffer-file-name))
-	 "rspec-snippets/"))
-
-;;; setup.el ends here
