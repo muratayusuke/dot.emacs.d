180004418b81751ebfc1b9e1289549bcc2493838
muratayusuke <info@muratayusuke.com>
2014-03-26 10:24:09 +0900

    fix recentf.el

--- inits/10-recentf.el
+++ inits/10-recentf.el
@@ -3,7 +3,11 @@
 (setq recentf-auto-cleanup 'never)
 (setq recentf-max-menu-items 500)
 (setq recentf-max-saved-items 500)
-(setq recentf-exclude '("^/[^/:]+:" "/svn-commit\.tmp$" "COMMIT_EDITMSG"
-						"/bookmarks$"))
+(add-to-list 'recentf-exclude "^/[^/:]+:")
+(add-to-list 'recentf-exclude "svn-commit\.tmp$")
+(add-to-list 'recentf-exclude "COMMIT_EDITMSG")
+(add-to-list 'recentf-exclude "bookmarks")
+(add-to-list 'recentf-exclude "\\.recentf")
+(add-to-list 'recentf-exclude "\\.revive\\.el")
 
 (recentf-mode 1)
