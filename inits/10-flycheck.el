1711f2543f1bc7a0141b5a7f5b131514edd85017
muratayusuke <info@muratayusuke.com>
2014-04-29 12:21:25 +0900

    enable flycheck rubocop after food critic

--- inits/10-flycheck.el
+++ inits/10-flycheck.el
@@ -1,5 +1,34 @@
+(require 'flycheck)
 (add-hook 'after-init-hook #'global-flycheck-mode)
 
 (eval-after-load 'flycheck
   '(custom-set-variables
    '(flycheck-display-errors-function #'flycheck-pos-tip-error-messages)))
+
+(defun flycheck-print-current-checker (args)
+  "Print checker for current buffer.
+ARGS is dummy"
+  (interactive "P")
+  (print (flycheck-get-checker-for-buffer))
+  )
+
+(flycheck-define-checker chef-foodcritic
+  "A Chef cookbooks syntax checker using Foodcritic.
+
+See URL `http://acrmp.github.io/foodcritic/'."
+  :command ("foodcritic" source)
+  :error-patterns
+  ((error line-start (message) ": " (file-name) ":" line line-end))
+  :modes (enh-ruby-mode ruby-mode)
+  :predicate
+  (lambda ()
+    (let ((parent-dir (f-parent default-directory)))
+      (or
+       ;; Chef CookBook
+       ;; http://docs.opscode.com/chef/knife.html#id38
+	   (print "hoge")
+       (locate-dominating-file parent-dir "recipes")
+       ;; Knife Solo
+       ;; http://matschaffer.github.io/knife-solo/#label-Init+command
+       (locate-dominating-file parent-dir "cookbooks"))))
+  :next-checkers ((warnings-only . ruby-rubocop)))
