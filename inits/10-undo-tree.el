5144bf0dca83ba418b261331466cbed36a388c0c
muratayusuke <info@muratayusuke.com>
2014-05-26 11:38:22 +0900

    change undo-tree-hist dir

--- inits/10-undo-tree.el
+++ inits/10-undo-tree.el
@@ -1,3 +1,13 @@
+(eval-after-load "undo-tree"
+  '(progn
+     (global-undo-tree-mode)
+     (setq undo-tree-auto-save-history t
+           undo-tree-history-directory-alist `(("." .,
+                                                (expand-file-name "~/.emacs.d/undo-tree-hist/"))))
+     (defadvice undo-tree-make-history-save-file-name
+       (after undo-tree activate)
+       (setq ad-return-value (concat ad-return-value ".gz")))))
+
 (require 'undo-tree)
 (global-undo-tree-mode)
 
