607214fdc8a9c528adc21427bda0f3eea3dbe0e4
muratayusuke <info@muratayusuke.com>
2014-06-26 00:38:11 -0700

    setup coffee

--- inits/10-coffee.el
+++ inits/10-coffee.el
@@ -1,3 +1,4 @@
 (add-to-list 'load-path "~/.emacs.d/elisp/coffee-mode/")
 (autoload 'coffee-mode "coffee-mode" "Major mode for editing CoffeeScript." t)
 (add-to-list 'auto-mode-alist '("\\.coffee$" . coffee-mode))
+(custom-set-variables '(coffee-tab-width 2))
