3df41500064975bb606f973b001720763a4c5c02
muratayusuke <info@muratayusuke.com>
2014-09-05 16:08:01 +0900

    set indent for js2-mode

--- inits/10-js2.el
+++ inits/10-js2.el
@@ -1,3 +1,9 @@
 (autoload 'js2-mode "js2-mode" nil t)
 (add-to-list 'auto-mode-alist '("\\.js$" . js2-mode))
 (add-to-list 'auto-mode-alist '("\\.json$" . js2-mode))
+
+(defun my-js2-mode-hook ()
+  (setq indent-tabs-mode nil
+		js2-basic-offset 2))
+
+  (add-hook 'js2-mode-hook 'my-js2-mode-hook)
