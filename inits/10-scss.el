afb85bd2449e26d6bbd78aef19350f027340b01e
muratayusuke <info@muratayusuke.com>
2014-12-30 03:36:02 -0800

    use 2 spaces for indent in scss-mode

--- inits/10-scss.el
+++ inits/10-scss.el
@@ -1,3 +1,9 @@
 (autoload 'scss-mode "scss-mode")
 (setq scss-compile-at-save nil)
 (add-to-list 'auto-mode-alist '("\\.scss\\'" . scss-mode))
+
+(add-hook 'scss-mode-hook
+		  #'(lambda ()
+			  (setq indent-tabs-mode nil)
+			  (set (make-local-variable 'css-indent-offset) 2)
+			  ))
