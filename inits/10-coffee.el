36bfdc478e9bd6902e19928b726648bc013b92f7
muratayusuke <info@muratayusuke.com>
2015-07-03 15:00:39 +0900

    use coffee-mode on .cjsx

--- inits/10-coffee.el
+++ inits/10-coffee.el
@@ -1,4 +1,5 @@
 (add-to-list 'load-path "~/.emacs.d/elisp/coffee-mode/")
 (autoload 'coffee-mode "coffee-mode" "Major mode for editing CoffeeScript." t)
 (add-to-list 'auto-mode-alist '("\\.coffee$" . coffee-mode))
+(add-to-list 'auto-mode-alist '("\\.cjsx$" . coffee-mode))
 (custom-set-variables '(coffee-tab-width 2))
