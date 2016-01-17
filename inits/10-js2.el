ac30669aef7db026ee1b04d7de3e0eb10740e0b6
muratayusuke <info@muratayusuke.com>
2016-01-17 14:32:32 +0900

    Use js2-mode in .jsx file

--- inits/10-js2.el
+++ inits/10-js2.el
@@ -1,6 +1,7 @@
 (autoload 'js2-mode "js2-mode" nil t)
 (add-to-list 'auto-mode-alist '("\\.js$" . js2-mode))
 (add-to-list 'auto-mode-alist '("\\.json$" . js2-mode))
+(add-to-list 'auto-mode-alist '("\\.jsx$" . js2-mode))
 
 (defun my-js2-mode-hook ()
   (setq indent-tabs-mode nil
