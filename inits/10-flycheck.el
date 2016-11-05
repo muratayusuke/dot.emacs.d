bc30800a948bc26896e645c06d0e62b014c1003c
muratayusuke <info@muratayusuke.com>
2016-11-06 00:35:08 +0900

    Fix flycheck error

--- inits/10-flycheck.el
+++ inits/10-flycheck.el
@@ -1,9 +1,9 @@
 (require 'flycheck)
+(require 'flycheck-pos-tip)
 (add-hook 'after-init-hook #'global-flycheck-mode)
 
-(eval-after-load 'flycheck
-  '(custom-set-variables
-   '(flycheck-display-errors-function #'flycheck-pos-tip-error-messages)))
+(with-eval-after-load 'flycheck
+  (flycheck-pos-tip-mode))
 
 (defun flycheck-print-current-checker (args)
   "Print checker for current buffer.
