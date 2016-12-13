013849832ca3e415e0e7937377e14f0ddeb9b515
muratayusuke <info@muratayusuke.com>
2016-12-13 13:32:24 +0900

    Update yasnippet to fix bug

--- elisp/elpa/helm-c-yasnippet-20160823.611/helm-c-yasnippet-autoloads.el
+++ elisp/elpa/helm-c-yasnippet-20160823.611/helm-c-yasnippet-autoloads.el
@@ -1,10 +1,10 @@
 ;;; helm-c-yasnippet-autoloads.el --- automatically extracted autoloads
 ;;
 ;;; Code:
-(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
 
-;;;### (autoloads nil "helm-c-yasnippet" "helm-c-yasnippet.el" (22550
-;;;;;;  57751 0 0))
+;;;### (autoloads nil "helm-c-yasnippet" "helm-c-yasnippet.el" (22607
+;;;;;;  31135 0 0))
 ;;; Generated autoloads from helm-c-yasnippet.el
 
 (autoload 'helm-yas-complete "helm-c-yasnippet" "\
