ae6c0d7119bed8f1a50188df4c6cb0c568bd71a4
muratayusuke <info@muratayusuke.com>
2017-02-22 11:46:58 +0900

    Auto-complete on scss-mode

--- inits/10-scss.el
+++ inits/10-scss.el
@@ -2,8 +2,12 @@
 (setq scss-compile-at-save nil)
 (add-to-list 'auto-mode-alist '("\\.scss\\'" . scss-mode))
 
+(defun configure-auto-complete-for-scss ()
+  (add-to-list 'ac-sources 'ac-source-css-property))
 (add-hook 'scss-mode-hook
 		  #'(lambda ()
 			  (setq indent-tabs-mode nil)
 			  (set (make-local-variable 'css-indent-offset) 2)
+              (configure-auto-complete-for-scss)
 			  ))
+(add-to-list 'ac-modes 'scss-mode)
